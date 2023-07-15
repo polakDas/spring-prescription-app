1b0c582dd24a167ba0e827b7f95dd218edebb81b
polakD <pulok3968@gmail.com>
2023-07-16 01:58:35 +0600

    initial commit

---
+++ src/main/java/com/medical/prescriptionapplication/PrescriptionApplication.java
@@ -0,0 +1,13 @@
+package com.medical.prescriptionapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrescriptionApplication.class, args);
+	}
+
+}
